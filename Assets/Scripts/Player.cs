774282039468ffd1c101da77e76686fbc4d47ce7
Pawel Nowak <p.n98@interia.pl>
2020-05-07 00:02:55 +0200

    Added horizontal movement for sprite

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private Rigidbody2D rigidBody;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontalInput = Input.GetAxisRaw("Horizontal");
+
+        rigidBody.velocity = new Vector2(horizontalInput, rigidBody.velocity.y);
+    }
+}
