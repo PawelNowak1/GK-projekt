fbc65d8977df13f596fb8abbf24ba55f53aedb7d
Adam Wolny <adamwolny98@gmail.com>
2020-05-17 14:17:17 +0200

    Add menu screens

---
+++ Assets/Scripts/ChooseMap.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChooseMap : MonoBehaviour
+{
+    public void FirstMap()
+    {
+        SceneManager.LoadScene("SampleScene2");
+    }
+
+    public void GoBack()
+    {
+        SceneManager.LoadScene("StartGame");
+    }
+}
