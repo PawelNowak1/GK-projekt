10c88d05e7f183228d545fb72e4ae5220bde59e7
Adam Wolny <adamwolny98@gmail.com>
2020-05-18 23:20:09 +0200

    Timer

--- Assets/Scripts/Finish.cs
+++ Assets/Scripts/Finish.cs
@@ -5,11 +5,15 @@ using UnityEngine.SceneManagement;
 
 public class Finish : MonoBehaviour
 {
+    public GameObject finishUI;
+    public GameManager gameManager;
+
     private void OnTriggerEnter2D(Collider2D other)
     {
         if(other.tag == "Player")
         {
-            SceneManager.LoadScene("StartGame");
+            gameManager.FinishMap();
+            finishUI.SetActive(true);
         }
     }
 }
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class GameManager : MonoBehaviour
+{
+    public TextMeshProUGUI timerText;
+    public TextMeshProUGUI endTimerText;
+
+    private float startTime;
+    private bool isActive;
+
+    void Start ()
+    {
+        startTime = Time.time;
+        isActive = true;
+    }
+
+    void Update()
+    {
+        if(isActive){
+            float t = Time.time - startTime;
+
+            string minutes = ((int)t / 60).ToString();
+            string seconds = (t % 60).ToString("f0");
+
+            if(seconds.Length > 1)
+            {
+                timerText.text = minutes + ":" + seconds;
+            }
+            else
+            {
+                timerText.text = minutes + ":0" + seconds;
+            }
+            
+        }
+    }
+      
+
+    public void FinishMap()
+    {
+        isActive = false;
+        float t = Time.time - startTime;
+
+        string minutes = ((int)t / 60).ToString();
+        string seconds = (t % 60).ToString("f0");
+
+        if (seconds.Length > 1)
+        {
+            endTimerText.text = minutes + ":" + seconds;
+        }
+        else
+        {
+            endTimerText.text = minutes + ":0" + seconds;
+        }
+    }
+}
