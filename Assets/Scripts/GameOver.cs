e6de2b735ad4a657b86f4a9138d0c85197ea9e30
Adam Wolny <adamwolny98@gmail.com>
2020-05-20 23:10:11 +0200

    Game over

---
+++ Assets/Scripts/GameOver.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameOver : MonoBehaviour
+{
+    public GameObject gameOverUI;
+    public GameManager gameManager;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            gameManager.FinishMap();
+            gameOverUI.SetActive(true);
+        }
+    }
+
+    public void MenuButton()
+    {
+        SceneManager.LoadScene("ChooseMap");
+    }
+
+    public void RestartButton()
+    {
+        SceneManager.LoadScene("SampleScene2");
+    }
+}
