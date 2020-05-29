e21d5a72c298ddee8dd9309649ec63919d9c55a3
Pawel Nowak <p.n98@interia.pl>
2020-05-29 15:33:11 +0200

    Added Enemy Sprite, animation, movement Rogue6

---
+++ Assets/Scripts/Enemy/Rogue06.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rogue06 : Enemy
+{
+    private Vector3 currentTarget;
+    private Animator anim;
+    private SpriteRenderer[] sprites;
+
+    private void Start()
+    {
+        anim = GetComponentInChildren<Animator>();
+        sprites = GetComponentsInChildren<SpriteRenderer>();
+    }
+
+    public override void Update()
+    {
+        if (anim.GetCurrentAnimatorStateInfo(0).IsName("Rogue_idle_01"))
+        {
+            return;
+        }
+
+        movement();
+    }
+
+    void movement()
+    {
+        if (currentTarget == pointA.position && transform.rotation.eulerAngles.y == 0)
+        {
+            // rogueSprite.flipX = true;
+            //transform.rotation = new Vector3(0, 180, 0);
+            transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles.x, 180, transform.rotation.eulerAngles.z);
+
+        }
+        else if (currentTarget == pointB.position && transform.rotation.eulerAngles.y == 180)
+        {
+            //rogueSprite.flipX = false;
+            transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles.x, 0, transform.rotation.eulerAngles.z);
+        }
+
+        if (transform.position == pointA.position)
+        {
+            currentTarget = pointB.position;
+            anim.SetTrigger("Idle");
+        }
+        else if (transform.position == pointB.position)
+        {
+            currentTarget = pointA.position;
+            anim.SetTrigger("Idle");
+        }
+        transform.position = Vector3.MoveTowards(transform.position, currentTarget, speed * Time.deltaTime);
+
+    }
+}
